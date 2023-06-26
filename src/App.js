6f4e04f61e90cda47ef9c15d61cfc83d80a0b32c
vaishnavihole <vaishnavihole1@gmail.com>
2023-06-26 09:38:06 +0530

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
     <h1 className='app-header'>Chocolate App</h1>
-
       <Chocolate title="Kharbuj" description="Khabuj is fruit" price="10"/>
       <Chocolate title="Dairy Milk" description="dairy milk is sweet" price="10"/>
       <Chocolate title="Perk" description="perk is chocolate" price="10"/>
